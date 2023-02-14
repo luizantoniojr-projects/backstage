702dddf7f83cc94abc408a875e3f35c0b3f1e617
silvajuniorluizantonio <silvajuniorluizantonio@outlook.com>
2023-02-13 22:55:58 -0300

    .

--- src/packages/backend/src/plugins/auth.ts
+++ src/packages/backend/src/plugins/auth.ts
@@ -26,16 +26,12 @@ export default async function createPlugin(
                   'Login failed, user profile does not contain an email',
                 );
               }
-              // We again use the local part of the email as the user name.
               const [localPart] = profile.email.split('@');
-
-              // By using `stringifyEntityRef` we ensure that the reference is formatted correctly
               const userEntityRef = stringifyEntityRef({
                 kind: 'User',
                 name: localPart,
                 namespace: DEFAULT_NAMESPACE,
               });
-
               return ctx.issueToken({
                 claims: {
                   sub: userEntityRef,
